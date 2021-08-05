441f2d42e05b4f8d52d16e4fe9ae8366093ff6bb
Rayan <c204rayan@gmail.com>
2021-08-06 02:12:25 +0530

    first

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Form'`, () => {
+  it(`should have as title 'register-form'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Form');
+    expect(app.title).toEqual('register-form');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Form app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('register-form app is running!');
   });
 });
